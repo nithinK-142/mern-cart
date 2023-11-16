04ea91a556185af6f692056a1429fce76647ea17
nithinK-142 <nithin.sagar.359@gmail.com>
2023-11-16 18:22:19 +0530

    update: mongodburl to env.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,14 @@
+import { config } from "dotenv";
+config();
+
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import { userRouter } from "./routes/user";
 import { productRouter } from "./routes/product";
-import { config } from "dotenv";
 
-config();
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(
@@ -22,9 +24,7 @@ app.use("/product", productRouter);
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(
-      `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@ecommerce.td2to3g.mongodb.net/ecommerce`
-    );
+    await mongoose.connect(`${process.env.MONGODB_URL}`);
     console.log("MongoDB connected");
   } catch (error) {
     console.error(`ERROR: ${error}`);
@@ -34,7 +34,11 @@ const connectDB = async () => {
 connectDB();
 
 app.get("/test", (_, res) => {
-  res.send("Hey there");
+  res.send(PORT);
 });
 
-app.listen(3001, () => console.log("SERVER STARTED"));
+app.listen(PORT, () => console.log("SERVER STARTED"));
+
+// await mongoose.connect(
+//   `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@ecommerce.td2to3g.mongodb.net/${process.env.MONGODB_CLUSTERNAME}`
+// );
