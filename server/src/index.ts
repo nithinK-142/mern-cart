62f93a48a7194c3b30561bdf7a2fdf27febbc209
nithinK-142 <nithin.sagar.359@gmail.com>
2023-11-16 20:18:49 +0530

    update: cors origin

--- server/src/index.ts
+++ server/src/index.ts
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(
   cors({
-    origin: [`${process.env.URL}`],
+    origin: ["https://mern-cart.vercel.app"],
     methods: ["POST", "GET"],
     credentials: true,
   })
