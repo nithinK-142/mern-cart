75cd05c743373ffe78923f363f40e9bd5e3fd0a4
nithinK-142 <nithin.sagar.359@gmail.com>
2023-11-16 20:39:24 +0530

    update: accessing cors origin from env.

--- server/build/index.js
+++ server/build/index.js
@@ -12,17 +12,18 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = require("dotenv");
+(0, dotenv_1.config)();
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const mongoose_1 = __importDefault(require("mongoose"));
 const user_1 = require("./routes/user");
 const product_1 = require("./routes/product");
-const dotenv_1 = require("dotenv");
-(0, dotenv_1.config)();
 const app = (0, express_1.default)();
+const PORT = process.env.PORT || 3001;
 app.use(express_1.default.json());
 app.use((0, cors_1.default)({
-    origin: ["https://mern-cart.vercel.app"],
+    origin: [`${process.env.URL}`],
     methods: ["POST", "GET"],
     credentials: true,
 }));
@@ -30,7 +31,7 @@ app.use("/user", user_1.userRouter);
 app.use("/product", product_1.productRouter);
 const connectDB = () => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        yield mongoose_1.default.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@ecommerce.td2to3g.mongodb.net/ecommerce`);
+        yield mongoose_1.default.connect(`${process.env.MONGODB_URL}`);
         console.log("MongoDB connected");
     }
     catch (error) {
@@ -41,4 +42,4 @@ connectDB();
 app.get("/test", (_, res) => {
     res.send("Hey there");
 });
-app.listen(3001, () => console.log("SERVER STARTED"));
+app.listen(PORT, () => console.log("SERVER STARTED"));
