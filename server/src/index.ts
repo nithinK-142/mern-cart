c14cacd12ccc79979b523eba6c902b3f06516404
nithinK-142 <nithin.sagar.359@gmail.com>
2023-12-01 18:16:15 +0530

    update: local server initialization.

--- server/src/index.ts
+++ server/src/index.ts
@@ -11,13 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
-app.use(
-  cors({
-    origin: [`${process.env.URL}`],
-    methods: ["POST", "GET"],
-    credentials: true,
-  })
-);
+app.use(cors({}));
 
 app.use("/user", userRouter);
 app.use("/product", productRouter);
