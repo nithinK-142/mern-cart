ff92c9181566840667122fe62df7bdf6db549051
nithinK-142 <nithin.sagar.359@gmail.com>
2023-12-02 11:53:58 +0530

    update: cors url from env.

--- server/src/index.ts
+++ server/src/index.ts
@@ -11,7 +11,13 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
-app.use(cors({}));
+app.use(
+  cors({
+    origin: [`${process.env.URL}`],
+    methods: ["POST", "GET"],
+    credentials: true,
+  })
+);
 
 app.use("/user", userRouter);
 app.use("/product", productRouter);
