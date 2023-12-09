400a003d7802314a883559fc075c66676fc9f150
nithinK-142 <nithin.sagar.359@gmail.com>
2023-12-09 20:00:14 +0530

    feat: spinner.

---
+++ client/src/components/Spinner.tsx
@@ -0,0 +1,21 @@
+import "./Spinner.css";
+const Spinner = () => {
+  return (
+    <div className="spinner-gear-container">
+      <div className="spinner-gear">
+        <div className="gear">
+          <div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Spinner;
--- client/src/pages/shop/index.tsx
+++ client/src/pages/shop/index.tsx
@@ -4,12 +4,16 @@ import { Product } from "./Product";
 import "./style.css";
 import { IShopContext, ShopContext } from "../../context/shop-context";
 import { Navigate } from "react-router-dom";
+import Spinner from "../../components/Spinner";
 
 export const ShopPage = () => {
   const { products } = useGetProducts();
   const { isAuthenticated } = useContext<IShopContext>(ShopContext);
 
   if (!isAuthenticated) return <Navigate to="/auth" />;
+
+  if(products.length === 0) return <Spinner />
+
   return (
     <div className="shop">
       <div className="products">
