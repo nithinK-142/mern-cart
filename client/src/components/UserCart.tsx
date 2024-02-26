7176377c61e2122c6644b213bf3dcd2841dea4d0
nithinK-142 <nithin.sagar.359@gmail.com>
2024-02-26 11:52:07 +0530

    refactor: using dropdown for cart notification center

--- client/src/components/UserCart.tsx
+++ client/src/components/UserCart.tsx
@@ -1,8 +1,7 @@
 import { ShopContext } from "@/context/shop-context";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { icons } from "@/assets/icons";
-import { HoverCard, HoverCardContent, HoverCardTrigger } from "./ui/hover-card";
 import { Button } from "./ui/button";
 import {
   Dialog,
@@ -14,13 +13,32 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "./ui/dialog";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { CreditCardIcon, XCircleIcon } from "lucide-react";
 
 const UserCart = () => {
   const { cartItemCount, clearCart } = useContext(ShopContext);
+  const [cartLogs] = useState([
+    {
+      title: "item added to the cart.",
+    },
+    {
+      title: "cart cleared.",
+    },
+  ]);
+
   return (
     <Dialog>
-      <HoverCard openDelay={100}>
-        <HoverCardTrigger>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
           <div className="relative w-6 h-6 cursor-pointer dark:fill-white">
             {icons.cart}
             {cartItemCount > 0 && (
@@ -29,33 +47,37 @@ const UserCart = () => {
               </span>
             )}
           </div>
-        </HoverCardTrigger>
-        <HoverCardContent className="pb-2">
-          <div className="border-b border-gray-300 dark:border-gray-100/50">
-            <h2 className="pb-1 font-medium text-center">Cart</h2>
-          </div>
-          <div className="flex flex-col py-4 pb-2">
-            <div className="flex items-center justify-between">
-              <span className="pl-2 text-sm">item added to cart</span>
-              <span className="inline-block p-1 bg-blue-600 rounded-full" />
-            </div>
-          </div>
-          <div className="border-t border-gray-300 dark:border-gray-100/50" />
-          <div className="relative py-4 mt-2">
-            <Button variant={"secondary"} className="absolute left-0 bottom-1">
-              <Link to="/checkout">go to checkout</Link>
-            </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-56">
+          <DropdownMenuLabel className="text-center">Cart</DropdownMenuLabel>
+
+          <DropdownMenuSeparator />
+
+          {cartLogs.map(({ title }) => (
+            <DropdownMenuGroup>
+              <DropdownMenuItem className="flex items-center justify-between">
+                <span>{title}</span>
+                <span className="inline-block p-1 bg-blue-600 rounded-full cursor-pointer" />
+              </DropdownMenuItem>
+            </DropdownMenuGroup>
+          ))}
+
+          <DropdownMenuSeparator />
+
+          <DropdownMenuGroup className="flex justify-evenly">
+            <DropdownMenuItem className="flex space-x-2">
+              <Link to="/checkout">checkout</Link>
+              <CreditCardIcon className="w-4 h-4 mr-2" />
+            </DropdownMenuItem>
             <DialogTrigger asChild>
-              <Button
-                variant={"secondary"}
-                className="absolute right-0 bottom-1"
-              >
-                Clear cart
-              </Button>
+              <DropdownMenuItem className="flex space-x-2">
+                <span> clear cart</span>
+                <XCircleIcon className="w-4 h-4 mr-2" />
+              </DropdownMenuItem>
             </DialogTrigger>
-          </div>
-        </HoverCardContent>
-      </HoverCard>
+          </DropdownMenuGroup>
+        </DropdownMenuContent>
+      </DropdownMenu>
 
       <DialogContent>
         <DialogHeader>
