31ea4623e3f69ecf1a43fc5cd05c693cd52f050a
nithinK-142 <nithin.sagar.359@gmail.com>
2024-03-11 19:14:07 +0530

    update: denying access to authpage to authenticated user.

--- client/src/pages/auth/index.tsx
+++ client/src/pages/auth/index.tsx
@@ -1,8 +1,14 @@
 import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import Login from "./login";
 import Register from "./register";
+import { Navigate } from "react-router-dom";
+import { ShopContext } from "@/context/shop-context";
+import { useContext } from "react";
 
 const AuthPage = () => {
+  const { isAuthenticated } = useContext(ShopContext);
+  if (isAuthenticated) return <Navigate to="/shop" />;
+
   return (
     <div className="flex justify-center mt-28">
       <Tabs defaultValue="login" className="w-[340px] sm:w-[400px] h-[400px]">
