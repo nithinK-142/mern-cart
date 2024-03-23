88a5fd3a4566b2e081d7152f22439bf3fd9135c7
nithinK-142 <nithin.sagar.359@gmail.com>
2024-03-23 11:20:26 +0530

    add: helmet middleware.

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,11 +4,23 @@ config();
 import express, { Request, Response } from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import helmet from "helmet";
 import { userRouter } from "./routes/user";
 import { productRouter } from "./routes/product";
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.use(helmet());
+
+// app.use(helmet.contentSecurityPolicy({
+//   directives: {
+//     defaultSrc: ["'self'"],
+//     scriptSrc: ["'self'", 'example.com'],
+//     objectSrc: ["'none'"],
+//     upgradeInsecureRequests: [],
+//   },
+// }));
 
 app.use(express.json());
 app.use(
