232e4c5b7bdd2bbe19dad38bfd1b21177c330f35
nithinK-142 <nithin.sagar.359@gmail.com>
2024-06-05 13:00:20 +0530

    add: verify token middleware.

---
+++ server/src/middleware/verifyToken.middleware.ts
@@ -0,0 +1,20 @@
+import type { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+
+export const verifyToken = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const authHeader = req.headers.authorization;
+  if (authHeader) {
+    jwt.verify(authHeader, process.env.SECRET as string, (err) => {
+      if (err) {
+        return res.sendStatus(403);
+      }
+      next();
+    });
+  } else {
+    return res.sendStatus(401);
+  }
+};
